dca42a873a8009b24eda72800cfd93c05538eac8
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-09 23:33:37 +0530

    Add files via upload

---
+++ DoorSecuritySystemApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSecuritySystemApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemSecurity systemSecurity = new SystemSecurity();
+            DoorStatusHandler _addressOfNotification = new DoorStatusHandler(systemSecurity.Notification);
+
+
+            DoorSensor _doorSensor = new DoorSensor();
+            _doorSensor.OnDoorStatusChanged += _addressOfNotification;
+            _doorSensor.DoorOpean();
+            _doorSensor.DoorClosed();
+            Console.ReadKey();
+        }
+    }
+}
