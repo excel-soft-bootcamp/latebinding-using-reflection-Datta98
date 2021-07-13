487f8080709a5bb134441c7612ba91e519eca641
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-13 15:34:03 +0530

    Add files via upload

---
+++ DoorSecuritySystemApplication/DoorSensor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DoorSecuritySystemApplication
+{
+
+    public delegate void DoorStatusHandler(string status);
+     public class DoorSensor
+    {
+        string doorStatus ;
+        public event DoorStatusHandler OnDoorStatusChanged;
+        public void  DoorOpean()
+        {
+            doorStatus = "opened";
+
+            this.Notify();
+
+        }
+        public void DoorClosed()
+        {
+            doorStatus = "closed";
+            Thread.Sleep(3000);
+            this.Notify();
+        }
+
+        public void Notify()
+        {
+            string message = $"Door {this.doorStatus} and Time : {System.DateTime.Now.ToString()}";
+            OnDoorStatusChanged.Invoke(message);
+          
+        }
+
+
+    }
+}
---
+++ DoorSecuritySystemApplication/SystemSecurity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSecuritySystemApplication
+{
+    class SystemSecurity
+    {
+      public void Notification(string status)
+        {
+            
+            Console.WriteLine(status);
+        }
+    }
+}
